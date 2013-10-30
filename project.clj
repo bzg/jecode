2a97499e0854465d1a565e42a24199712f4d33c6
Bastien Guerry <bzg@altern.org>
2013-10-30 15:57:07 +0100

    Bump dependancies versions

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://github.com/bzg/jecode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :immutant {:context-path "/"}
+  :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [lib-noir "0.7.1"]
-   [selmer "0.4.8"]
-   [compojure "1.1.5"]
+   [lib-noir "0.7.3"]
+   [selmer "0.5.0"]
+   [compojure "1.1.6"]
    [ring-server "0.3.0"]
    [markdown-clj "0.9.33"]]
   :url "http://jecode.org"
