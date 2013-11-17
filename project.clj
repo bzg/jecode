73b882e5671f9dfbdd05daf518bc28f45fb5e833
Bastien Guerry <bzg@altern.org>
2013-11-17 08:58:24 +0100

    Bump versions

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [lib-noir "0.7.3"]
-   [selmer "0.5.0"]
+   [lib-noir "0.7.5"]
+   [selmer "0.5.2"]
    [compojure "1.1.6"]
-   [ring-server "0.3.0"]
-   [markdown-clj "0.9.33"]]
+   [ring-server "0.3.1"]
+   [markdown-clj "0.9.35"]]
   :url "http://jecode.org"
   :description "jecode.org"
   :min-lein-version "2.0.0")
