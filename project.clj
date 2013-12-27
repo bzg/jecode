1afcaee878be2508f9780da7acb6cfdf7e358489
Bastien Guerry <bzg@altern.org>
2013-12-27 10:12:03 +0100

    project.clj: Update libraries

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [lib-noir "0.7.8"]
+   [lib-noir "0.7.9"]
    [selmer "0.5.4"]
    [compojure "1.1.6"]
    [ring-server "0.3.1"]
-   [markdown-clj "0.9.36"]]
+   [markdown-clj "0.9.38"]]
   :url "http://jecode.org"
   :description "jecode.org"
   :min-lein-version "2.0.0")
