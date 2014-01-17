1658623ba99d7c5142a6678130857019c44db99c
Bastien Guerry <bzg@altern.org>
2014-01-17 17:58:25 +0100

    Bump selmer and markdown-clj versions

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [lib-noir "0.7.9"]
-   [selmer "0.5.4"]
+   [selmer "0.5.8"]
    [compojure "1.1.6"]
    [ring-server "0.3.1"]
-   [markdown-clj "0.9.38"]]
+   [markdown-clj "0.9.40"]]
   :url "http://jecode.org"
   :description "jecode.org"
   :min-lein-version "2.0.0")
