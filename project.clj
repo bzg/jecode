238e56afa0d5872cb4f7ace18e2df6b12f33c559
Bastien Guerry <bzg@altern.org>
2014-02-05 12:51:10 +0100

    Don't change context-path

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/bzg/jecode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [lib-noir "0.8.0"]
@@ -11,6 +10,5 @@
    [compojure "1.1.6"]
    [ring-server "0.3.1"]
    [markdown-clj "0.9.41"]]
-  :url "http://jecode.org"
   :description "jecode.org"
   :min-lein-version "2.0.0")
